a4165f642d5724a73e46ec0dadc08e9987e53a6a
SudarsanGouda-Adobe <inf16883@adobe.com>
2026-09-08 09:49:47 +0530

    for footer target blank link

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -5,6 +5,8 @@ import { loadFragment } from '../fragment/fragment.js';
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
+
+
 export default async function decorate(block) {
   // load footer as fragment
   const footerMeta = getMetadata('footer');
@@ -16,5 +18,23 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
+
+footer.querySelectorAll('a').forEach((link) => {
+  const href = link.href.toLowerCase();
+
+  if (
+    href.includes('twitter.com') ||
+    href.includes('instagram.com') ||
+    href.includes('facebook.com') ||
+    href.includes('linkedin.com')
+  ) {
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+  }
+});
+
+
   block.append(footer);
 }
+
+
